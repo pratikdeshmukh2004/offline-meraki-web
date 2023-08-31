7d4a6679d7d33de24163d4924d474562c9195dbe
saloni-080601 <salonipanwar532@gmail.com>
2023-08-31 14:21:39 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Component/Header'; // Import Header component
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Meraki</h1>
+      <Header/>
     </div>
   );
 }
