e921f07a06d800418bdeb9d5e7af39bbd78337aa
SanjnaPanwar <sanjnapanwar7697@gmail.com>
2023-08-31 15:52:57 +0530

    card of courses added

--- src/Component/Course/index.js
+++ src/Component/Course/index.js
@@ -1,25 +1,28 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Box from '@mui/material/Box';
 
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 function Course() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     // Define the API URL
-    const apiUrl = 'https://merd-api.merakilearn.org/pathways/1';
+    const apiUrl = `https://merd-api.merakilearn.org/pathways/1`;
 
     // Define the headers
     const headers = {
-      "version-code": 67
+      "version-code": 61
     };
 
-    // Fetch data from the API with custom headers
-    fetch(apiUrl, {
-      method: "GET",
-      headers: headers
-    })
-      .then(response => response.json())
-      .then(data => {
-        setData(data); // Update the state with fetched data
+    // Fetch data from the API using axios
+    axios.get(apiUrl, { headers })
+      .then(response => {
+        setData(response.data); // Update the state with fetched data
       })
       .catch(error => {
         console.error('Error fetching data:', error);
@@ -31,18 +34,30 @@ function Course() {
       // Perform any cleanup operations here
     };
   }, []);
-
+  const bull = (
+    <Box
+      component="span"
+      sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+    >
+    </Box>
+  );
   return (
     <div>
-      <h1>Pathways</h1>
+      <h1>Courses</h1>
       <div className="course-cards">
-        {data.courses.map(course => (
-          <div key={course.id} className="course-card">
-            <img src={course.logo} alt={course.name} />
-            <h2>{course.name}</h2>
-            <p>{course.short_description}</p>
-          </div>
-        ))}
+        {data.pathway?.courses.map(course => (
+          <Card sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                <img src={course.logo} alt={course.name} />
+              </Typography>
+              <Typography variant="h5" component="div">
+              {bull}{course.name}
+              </Typography>
+            </CardContent>
+          </Card>
+        )
+        )}
       </div>
     </div>
   );
