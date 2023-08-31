184d0ece15bfd11d08f80bb1e69d912893be1004
SanjnaPanwar <sanjnapanwar7697@gmail.com>
2023-08-31 19:01:07 +0530

    card

--- src/Component/Course/index.js
+++ src/Component/Course/index.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Box from '@mui/material/Box';
-import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-
+import "../../App.css"
 
 function CourseCard({ course }) {
   const bull = (
@@ -18,33 +16,24 @@ function CourseCard({ course }) {
     </Box>
   );
 
-  const cardStyle = {
-    width: "250px",    // Set the width of the card
-    height: "200px",   // Set the height of the card
-  };
-
-  const imgStyle = {
-    width: "100%",      // Make the image occupy the full width of the card
-    height: "100%",     // Make the image occupy the full height of the card
-    objectFit: "cover", // Maintain aspect ratio and cover the whole space
-  };
-
   return (
-    <Card style={cardStyle}>
-      <CardContent>
-        <Typography variant="h6" component="div">
-          <img
-            src={course.logo}
-            alt={course.name}
-            style={imgStyle} // Apply the image dimensions
-          />
-        </Typography>
-        <Typography variant="h6" component="div">
-          {bull}
-          {course.name}
-        </Typography>
-      </CardContent>
-    </Card>
+    <div className="card-container"> {/* Apply the card-container class */}
+      <Card>
+        <CardContent className="card-content"> {/* Apply the card-content class */}
+          <Typography variant="h6" component="div">
+            <img
+              src={course.logo}
+              alt={course.name}
+              className="card-image" // Apply the card-image class
+            />
+          </Typography>
+          <Typography variant="h6" component="div">
+            {bull}
+            {course.name}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
 function Course() {
@@ -78,7 +67,7 @@ function Course() {
   return (
     <div>
       <h1>Courses</h1>
-      <Grid container spacing={2}>
+      <Grid container spacing={2} className="grid-container">
         {data.pathway?.courses.map(course => (
           <Grid item xs={6} sm={4} md={3} key={course.id}>
             <CourseCard course={course} />
