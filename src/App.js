cd8d50a36b10ac9715a71283b3220ef914ff8d2b
Pratik <pratikdeshmukhlobhi@gmail.com>
2023-08-31 17:20:44 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [userData, setUserData] = useState(null);
+  const username = 'pratikdeshmukh2004'; // GitHub username
+
+  useEffect(() => {
+    async function fetchUserData() {
+      try {
+        const response = await axios.get(`https://api.github.com/users/${username}`);
+        setUserData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchUserData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>GitHub User App</h1>
+      {userData ? (
+        <div>
+          <img src={userData.avatar_url} alt={userData.login} />
+          <h2>{userData.login}</h2>
+          <p>{userData.bio}</p>
+          {/* Other user details */}
+        </div>
+      ) : (
+        <p>Loading user data...</p>
+      )}
     </div>
   );
 }
